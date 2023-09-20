8e2b6fa8790c8d4530639912471405d46510fbb8
Cody McCodePants <cody@mccodepants.com>
2023-09-19 22:30:32 -0500

    Board Drawn

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,27 @@
 #include "iostream"
 
+#include "vector"
+
+void drawBoard(std::vector<std::vector<char>> values) {
+    int line = 0;
+    for (int i = 0; i <= 3+1; i++) {
+        if (i % 2 == 0) {
+            std::cout << values[line][0] << " # " << values[line][1] << " # " << values[line][2] << std::endl; //TODO: Make Scalable
+            line++;
+        } else
+            std::cout << "#########" << std::endl;
+    }
+
+}
+
 int main() {
-    std::cout << "Hello world" << std::endl;
+    //TODO: Draw Board
+    // TODO: Create Board
+    std::vector<std::vector<char>> board = {{'X', 'O', 'X'},
+                                            {'X', 'X', 'X'},
+                                            {'X', 'O', 'O'}};
+
+
+    drawBoard(board);
+
 }
